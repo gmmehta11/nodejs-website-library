fad6bdfacb1b8a02ad185ea71c92aa8520c7602b
Gaurav <gmmehta1111@gmail.com>
2020-07-08 12:11:39 +0530

    Added form for creating new Author.

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const Author = require('../models/author')
 
 // All authors route
 router.get('/', (req, res) => {
@@ -10,11 +10,11 @@ router.get('/', (req, res) => {
 
 //New Author Route
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', { author: new Author() })
 })
 
 // Create Author Route
-router.post('/', (res, req) => {
+router.post('/', (req, res) => {
     res.send('Create')
 })
 
